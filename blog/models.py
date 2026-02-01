ac94bd512ec518afee779dec17b0baf157bf6828
tolase <tolasedairo@gmail.com>
2026-02-01 14:06:41 +0000

    feat: Enhance blog functionality with comments and improved post display

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,13 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 #implementing comments model
 class Comment(models.Model):
@@ -27,3 +34,11 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.body} | written by {self.author}"
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def blog_home(request):
-    return HttpResponse("CodeStar Blog is live!")
+    posts = Post.objects.filter(status=1).order_by('-created_on')
+    output = '<h1>CodeStar Blog</h1>'
+    for post in posts:
+        output += f'<h2>{post.title}</h2>'
+        output += f'<p>By {post.author} on {post.created_on}</p>'
+        output += f'<p>{post.excerpt}</p>'
+        output += '<hr>'
+    return HttpResponse(output)
