a2240795818f7535c52b20573e6fcc553f2c6feb
tolase <tolasedairo@gmail.com>
2026-02-02 00:28:25 +0000

    feat: Filter posts by status in PostList view

--- blog/views.py
+++ blog/views.py
@@ -17,5 +17,5 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
