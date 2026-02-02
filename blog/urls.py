57cde6fde91b0632b2f9655c775c43e4750af1ed
tolase <tolasedairo@gmail.com>
2026-02-02 01:39:40 +0000

    fix: Update URL patterns for consistency and clarity in blog and codestar URL configurations

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
 from . import views
+from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path("", views.PostList.as_view(), name='home'),
 ]
